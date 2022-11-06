6145fc048eba91f82f2dc7d760c9e68f978e1295
humblefirm <38067691+humblefirm@users.noreply.github.com>
2022-11-07 04:33:42 +0900

    fallback url

--- pages/api/exchangeRate.js
+++ pages/api/exchangeRate.js
@@ -1,12 +1,10 @@
 export default async function handler(req, res) {
   const { fiatAmount, fiatName, tokenName } = req.query;
 
-  res
-    .status(200)
-    .json({
-      fiatAmount,
-      fiatName,
-      tokenName,
-      tokenAmount: String(fiatAmount * 10.03),
-    });
+  res.status(200).json({
+    fiatAmount,
+    fiatName,
+    tokenAmount: String(fiatAmount * 10.03),
+    tokenName,
+  });
 }
