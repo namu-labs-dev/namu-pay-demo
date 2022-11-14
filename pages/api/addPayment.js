959df80c58f12cf422bfbb274d3bad8beb8d8118
bsangs <tkdals6805@naver.com>
2022-11-15 02:28:02 +0900

    feat: Done real system

--- pages/api/addPayment.js
+++ pages/api/addPayment.js
@@ -1,8 +1,5 @@
 import addCors from "../../lib/addCors";
-import { addPayment, getOrder, getPublicKey, setOrder, updatePayment } from "../../lib/dataIO";
-import axios from "axios";
-import { getWebhookURL } from "../../lib/webhook";
-import { sleep } from "../../lib/utils";
+import {addPayment, getExchangeRate, getOrder, getPublicKey, setOrder, updatePayment} from "../../lib/dataIO";
 
 const EthCrypto = require("eth-crypto");
 
@@ -11,6 +8,7 @@ export default async function handler(req, res) {
   const { uuid, password } = req.query;
 
   const payment = await getOrder(uuid);
+  payment["usdPrice"] = 1 / (await getExchangeRate("USD")) * Number(payment.fiatPrice);
   payment["requestedAt"] = new Date().toISOString();
   payment["status"] = "requested";
 
@@ -25,75 +23,23 @@ export default async function handler(req, res) {
 
   const paymentId = await addPayment(signaturePayment.walletAddress, null);
 
-
-  sleep(10).then(async () => {
-    // TODO: temp receipt, status added, After remove -> updatePayment move
-    // TODO: 실패했을 때도 추가해줘야 함.
-    const payment = await getOrder(uuid);
-    const receipt = {
-      txid: "0x6d2fb62eeaccac786a5e4f92b1afb046efdbdf6fb8894332af46c5d697029eeb",
-      explorer: "https://polygonscan.com/tx/0x6d2fb62eeaccac786a5e4f92b1afb046efdbdf6fb8894332af46c5d697029eeb"
-    }
-
-    await setOrder(uuid, {
-      ...payment,
-      receipt,
-      status: "succeed",
-      approvedAt: new Date().toISOString(),
-      paymentId
-    });
-    // TODO: Webhook mockup. After remove
-    try {
-      await axios.post(await getWebhookURL(""), {
-        succeed: "succeed",
-        orderUuid: uuid,
-      });
-    } catch (e) {
-      console.error(e);
-    }
-  })
-    /* when Tx was succeed */
-    .then(async () => {
-      const enc_payment = await EthCrypto.encryptWithPublicKey(
-        publicKey,
-        JSON.stringify({
-          ...signaturePayment,
-          paymentId
-        })
-      );
-
-      await updatePayment(signaturePayment.walletAddress, paymentId, enc_payment);
-
-      // res.status(200).json({ paymentId: paymentId });
-    })
-    /* when Tx was failed */
-    .catch(async (e) => {
-      // do something, when Tx was failed
-      // TODO: 실패 이유 추가, 고도화 시 error message에 따른 실패 문구 분류 필요
-      await setOrder(uuid, {
-        ...payment,
-        receipt,
-        status: "failed",
-        falureMessage: "결제에 실패했습니다.",
-        approvedAt: new Date().toISOString(),
+  const enc_payment = await EthCrypto.encryptWithPublicKey(
+      publicKey,
+      JSON.stringify({
+        ...signaturePayment,
         paymentId
-      });
-      // TODO: Webhook mockup. After remove
-      try {
-        await axios.post(await getWebhookURL(""), {
-          succeed: "failed",
-          orderUuid: uuid,
-        });
-      } catch (e) {
-        console.error(e);
-      }
-    })
-    .finally(() => res.status(200).json({
-      ...payment // 지금은 모든 데이터 전송, 고도화 시 데이터 정제 필요
-      // paymentId: paymentId,
-      // succeedUrl: payment.urlFailure,
-      // failedUrl: payment.urlSuccess,
-    }))
+      })
+  );
+
+  await updatePayment(signaturePayment.walletAddress, paymentId, enc_payment);
+  await setOrder(uuid, {
+    ...payment,
+    paymentId
+  })
+
+  res.status(200).json({
+    ...payment // TODO: 지금은 모든 데이터 전송, 고도화 시 데이터 정제 필요
+  })
 }
 
 
---
+++ pages/api/alchemyWebhook.js
@@ -0,0 +1,64 @@
+import {getOrder, getPayments, setOrder} from "../../lib/dataIO";
+import {checkSumAddress, getBlockchainExplorer} from "../../lib/utils";
+import axios from "axios";
+import {getWebhookURL} from "../../lib/webhook";
+
+export default async function handler(req, res) {
+    let body = req.body;
+
+    if (typeof body === "string") body = JSON.parse(body);
+
+    const { hash: txid, from, chainId } = body.event.transaction;
+
+    const walletAddress = checkSumAddress(from);
+
+    let payments = await getPayments(walletAddress);
+
+    const payment = JSON.parse(payments.filter(p => {
+        return JSON.parse(p).txid === txid
+    }).pop());
+
+    const { uuid } = payment;
+
+    let order = await getOrder(uuid);
+
+    // send WAD Webhook
+
+    let status = "failed";
+    let failReason = "unknown";
+
+    if (body.type === "MINED_TRANSACTION") {
+        // TODO : txid 검증 코드
+        status = "succeed";
+        order = {
+            ...order,
+            approvedAt: new Date().toISOString(),
+            receipt: {
+                txid,
+                explorer: `${getBlockchainExplorer(Number(chainId))}${txid}`
+            }
+        }
+    } else {
+        failReason = "Transaction has dropped";
+    }
+
+    order = {
+        ...order,
+        status,
+        failReason: status === "failed" ? failReason : null
+    }
+
+    await setOrder(uuid, order);
+
+    try {
+        await axios.post(await getWebhookURL(order.companyName), {
+            succeed: status,
+            orderUuid: uuid,
+        });
+    } catch (e) {
+        console.error(e);
+        // TODO : retry send webhook
+    }
+
+    res.status(200).json({...order});
+}
