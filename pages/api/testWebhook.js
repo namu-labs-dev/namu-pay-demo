8dbff5f5ee8d57f0f982e013f72a00e6575ade36
bsangs <tkdals6805@naver.com>
2022-11-15 16:19:27 +0900

    feat: webhook Test

--- pages/api/testWebhook.js
+++ pages/api/testWebhook.js
@@ -1,6 +1,9 @@
+import {updateWebhookLog} from "../../lib/dataIO";
+
 export default async function handler(req, res) {
     const body = req.body;
 
+    console.log(await updateWebhookLog(body));
 
     res.status(200).json({ok:true});
 }
