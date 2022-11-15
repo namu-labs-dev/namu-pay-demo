0b1429e85c6941ef5bba88e7db5bab3b05b2bb30
KimJaekyoon <kjk704@gmail.com>
2022-11-15 14:11:31 +0900

    Update: added in game demo page

---
+++ pages/game/index.js
@@ -0,0 +1,49 @@
+import Head from "next/head";
+import { useRouter } from "next/router";
+// import { Tip } from "../../components/Tip";
+import { Container } from "../../components/Container";
+import "react-toastify/dist/ReactToastify.css";
+import { ToastContainer } from "react-toastify";
+import { toast } from "react-toastify";
+import axios from "axios";
+import { useEffect, useState } from "react";
+
+import InGame from "https://framer.com/m/inGame-ByOr.js@sl0ckCP1lbkdNdj86fhe"
+
+const hasLocal = true;
+
+const namupayURL = hasLocal ?
+    "http://localhost:3000" :
+    "https://namupay.namu-labs.dev";
+
+
+const test = () => {
+    console.log("success!!!")
+}
+
+export default function Game() {
+    return (
+        <>
+            <Head>
+                <title>NamuPAY</title>
+                <meta name="description" content="Modules" />
+                <link rel="icon" href="/favicon.png" />
+                <link rel="preconnect" href="https://fonts.googleapis.com" />
+                <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+                <link
+                    href="https://fonts.googleapis.com/css2?family=Noto+Sans+KR:wght@100;300;400;500;700;900&display=swap"
+                    rel="stylesheet"
+                />
+                <meta
+                    name="viewport"
+                    content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no"
+                />
+            </Head>
+            <Container>
+                <InGame
+                    openShop={() => test()} // 여기서 와드 오픈
+                />
+            </Container>
+        </>
+    )
+}
