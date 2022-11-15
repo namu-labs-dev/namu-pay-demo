cc623e3b7ea6f531342e19bb57f10b7f21e69096
bsangs <tkdals6805@naver.com>
2022-11-15 15:12:06 +0900

    fix: Remove duplicate make payment

--- pages/api/addPayment.js
+++ pages/api/addPayment.js
@@ -8,6 +8,14 @@ export default async function handler(req, res) {
   const { uuid, password } = req.query;
 
   const payment = await getOrder(uuid);
+
+  if (payment.paymentId) {
+    return res.status(200).json({
+      paymentId: -1,
+      error: "already paid"
+    });
+  }
+
   payment["usdPrice"] = 1 / (await getFiatExchangeRate("USD")) * Number(payment.fiatPrice);
   payment["requestedAt"] = new Date().toISOString();
   payment["status"] = "requested";
@@ -38,7 +46,7 @@ export default async function handler(req, res) {
   })
 
   res.status(200).json({
-    ...payment // TODO: 지금은 모든 데이터 전송, 고도화 시 데이터 정제 필요
+    paymentId,
   })
 }
 
